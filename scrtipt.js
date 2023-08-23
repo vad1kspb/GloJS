a6251986ab33a04272621dbb080757f96efa1a0e
vadikspb178 <vadikspb178@gmail.com>
2023-08-23 08:40:19 +0300

    Включили строкий режим. use strict

--- scrtipt.js
+++ scrtipt.js
@@ -1,11 +1,11 @@
+"use strict";
+
 let title;
 let screens;
 let screenPrice;
 let rollback;
 let fullPrice;
 let adaptive;
-// alert("Внимание Внимание!");
-console.log("Внимание Внимание!!!");
 
 /* 
 Задание урок 3 
